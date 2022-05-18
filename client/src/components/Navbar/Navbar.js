80e5b8c34ea8e4bc1ba4f63ecb0fdbc0de31d131
Marta Nara <marta.kos1230@gmail.com>
2022-05-18 15:15:56 +0200

    Add styles to Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,17 +1,36 @@
 import styles from './Navbar.module.scss';
 import Container from '../Container/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 
   return (
     <div className={styles.root}>
       <Container>
-        <p>This is the navbar</p>
+      <div className={styles.navBar}>
+        <div className={styles.logo}>
+          <img alt='logo' src='images/home/kurashi_logo.png' />
+        </div>
+        <div className={styles.navLinks}>
+          <ul>
+            <li><NavLink to='/'>New Arrivals</NavLink></li>
+            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/'>Kitchen</NavLink></li>
+            <li><NavLink to='/'>Craft</NavLink></li>
+            <li><NavLink to='/'>Fashion</NavLink></li>
+          </ul>
+          <div className={styles.userButtons}>
+            <button className={styles.button}><FontAwesomeIcon icon={faCartShopping} /></button>
+            <button className={styles.button}><FontAwesomeIcon icon={faUser} /></button>
+         </div>
+        </div>
+      </div>
       </Container>
     </div>
   );
 };
 
 export default Navbar;
-
 
