b60750951e7fde57713aa5ed50b4ec86ebd3f03d
Marta Nara <marta.kos1230@gmail.com>
2022-05-24 19:01:58 +0200

    Add styles to category view

--- client/src/components/Category/Category.js
+++ client/src/components/Category/Category.js
@@ -2,6 +2,8 @@ import styles from './Category.module.scss';
 import { useParams } from 'react-router';
 import { useSelector } from 'react-redux';
 import { getProductsByCategory } from '../../redux/productsRedux';
+import Container from '../Container/Container';
+import { Link } from 'react-router-dom';
 
 const Category = () => {
   const { name } = useParams();
@@ -9,15 +11,27 @@ const Category = () => {
   const products = useSelector(state => getProductsByCategory(state, name));
 
   return (
-    <div className={styles.root}>
-    </div>
+      <Container>
+        <div className={styles.root}>
+          <h2>{name}</h2>
+          <div className={styles.imgContainer}>
+          <img alt={name} src={`/images/categories/${name}.jpg`} className={styles.categoryCover}/>
+          </div>
+          <div className={styles.productContainer}>
+            {
+              products.map(product =>
+              <div key={product.id} className={styles.productBox}>
+              <Link to={`item/${product.name}`}><img alt={product.name} src={product.variants.main}/></Link>
+                  <p><Link to={`item/${product.name}`}>{product.name}</Link></p>
+                  <p>$ {product.price} USD</p>
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </Container>
   );
 };
 
 export default Category;
-
-
- // <h2>{name}</h2>
-// <img alt='table' src='images/home/pexels-pavel-danilyuk-7674538.jpg' />
-// <div className={styles.productsContainer}></div>
 
