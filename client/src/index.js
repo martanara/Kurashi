28c9ff36cf19650ece98aaa653f81816a4625979
Marta Nara <marta.kos1230@gmail.com>
2022-05-30 21:55:04 +0200

    Eslint fixes before deployment on Heroku

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
+import React from 'react';
+
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import { store } from './redux/store';
+
 import { Provider } from 'react-redux';
+
+import { store } from './redux/store';
+
+import App from './App';
+
 import './styles/normalize.scss';
 import './styles/global.scss';
 
-const container = document.querySelector('#root')
+const container = document.querySelector('#root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
-  );
+);
