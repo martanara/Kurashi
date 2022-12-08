0db0554a36d12f5bf962f95d8f934cac3f50f1cc
Marta Nara <marta.kos1230@gmail.com>
2022-12-08 18:20:30 +0100

    Fix bug

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,2 @@
-const fs = require('fs')
-fs.writeFileSync('./.env', `MONGODB_USERNAME=${process.env.MONGODB_USERNAME}\n``MONGODB_PASSWORD=${process.env.MONGODB_PASSWORD}\n`)
+const fs = require('fs');
+fs.writeFileSync('./.env', `MONGODB_PASSWORD=${process.env.MONGODB_PASSWORD}\nMONGODB_USERNAME=${process.env.MONGODB_USERNAME}\n`);
