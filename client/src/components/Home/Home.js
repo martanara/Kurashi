161f036f664f5cfbd74903435595e8fcf9ad3156
Marta Nara <marta.kos1230@gmail.com>
2022-12-08 19:55:30 +0100

    Add dotenv to server.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,9 +7,6 @@ import Highlights from '../Highlights/Highlights';
 import styles from './Home.module.scss';
 
 const Home = () => {
-  console.log(process.env.MONGODB_USERNAME);
-  console.log(process.env.MONGODB_USERNAME);
-  
   return (
     <div className={styles.root}>
       <Hero />
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 const productsRoutes = require('./routes/products.routes');
 const ordersRoutes = require('./routes/orders.routes');
